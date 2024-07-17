ef3544eed8c31b96eccf5df5e15dc9949ffbc165
Kristian Zarębski <krizar312@gmail.com>
2024-07-17 18:28:59 +0100

    Added Alacritty configuration

--- install.ps1
+++ install.ps1
@@ -1,2 +1,5 @@
 $NvimDestination = "$env:LOCALAPPDATA/nvim/"
+$AlacrittyDestination = "$env:APPDATA/alacritty"
 robocopy /s $PSScriptRoot/nvim/ $NvimDestination/
+New-Item -ItemType Directory -Force -Path $AlacrittyDestination
+robocopy $PSScriptRoot/alacritty/ $AlacrittyDestination/
