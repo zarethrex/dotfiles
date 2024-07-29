2b02d6454d359ed210d2eac987a8c62c260bc964
Kristian Zarębski <krizar312@gmail.com>
2024-07-29 07:55:57 +0100

    Fix luarocks and remove broken lua modules

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,4 @@
 require("config.lazy")
 require("lspconfig").ruff_lsp.setup({})
 require("code_runner").setup({})
-require("cmp").setup({
-	sources = {
-		{ name = "git" },
-		-- more sources
-	},
-})
-require("cmp_git").setup()
+require("luarocks-nvim").setup()
