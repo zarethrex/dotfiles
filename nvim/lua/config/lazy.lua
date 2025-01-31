b78ce2220e6112a3d7e6e4250407ae4c3400b60f
Kristian Zarebski <krizar312@gmail.com>
2025-01-31 20:58:13 +0000

    Added more tree sitter configurations

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -59,6 +59,14 @@ require("lazy").setup({
 		},
 	},
 })
+require("rust-tools").setup({
+	on_attach = function(_, bufnr)
+		-- Hover actions
+		vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+		-- Code action groups
+		vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+	end,
+})
 require("colorizer").setup({ "css", "markdown" })
 require("neotest").setup({
 	adapters = {
