064d9334b3baef9724d6ee360521d28a14395f9f
Kristian Zarębski <krizar312@gmail.com>
2025-06-10 22:06:17 +0100

    🐛 Added missing lua files

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,8 +8,8 @@ require("devcontainer").setup({})
 require("smoji")
 require("filetype")
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.py", -- Adjust the pattern to target specific file types
-  callback = function()
-    vim.lsp.buf.format({ async = false }) -- Sync formatting before saving
-  end,
+	pattern = "*.py", -- Adjust the pattern to target specific file types
+	callback = function()
+		vim.lsp.buf.format({ async = false }) -- Sync formatting before saving
+	end,
 })
