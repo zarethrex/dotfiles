1d99de0be76925bfd1b04a05093a71a6e6b4c4aa
Kristian Zarębski <krizar312@gmail.com>
2025-06-19 15:06:22 +0100

    🏷️ Added additional file types
    Added additional file types to Neovim

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -1,6 +1,7 @@
 vim.filetype.add({
 	pattern = {
 		[".*/roles/.*/tasks/.*%.yml"] = "yaml.ansible",
+		[".*/roles/.*/handlers/.*%.yml"] = "yaml.ansible",
 		[".*/molecule/.*/converge.yml"] = "yaml.ansible",
 		[".*/molecule/.*/verify.yml"] = "yaml.ansible",
 		[".*/molecule/.*/create.yml"] = "yaml.ansible",
